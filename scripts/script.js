5ba5716c8b1fead10a989f55f34ef419a00d9447
Samuel Suja <samuel.sirius@gmail.com>
2023-03-13 15:38:34 +0100

    Product detail + categories

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,32 @@ function toggleNav() {
     let isClose = document.getElementById("sidebar").style.width === "0px";
     document.getElementById("sidebar").style.width = isClose ? "250px" : "0";
 }
+
+const categoryTitleMapping =
+{
+    "bestseller": "Bestsellery",
+    "news": "Novinky",
+    "youngadult": "Young Adult",
+    "romance": "Romantika",
+    "crime": "Krimi a detektívky",
+    "thriller": "Trilery",
+    "adventure": "Dobrodružne",
+    "family": "Rodina",
+    "fantasy": "Fantasy",
+    "scifi": "Sci-fi",
+    "novels": "Romány a novely",
+    "biography": "Biografie",
+    "poetry": "Poézia",
+    "lifestyle": "Životný štýl",
+    "children": "Deti a mládež",
+    "education": "Náučná a odborná literatúra"
+}
+
+function setCategoryTitle()
+{
+    const url = new URL(window.location.toString());
+    console.log(url);
+    const categoryName = url.searchParams.get("category");
+    var categoryTitleElement = document.getElementById("category-title");
+    categoryTitleElement.innerHTML = categoryTitleMapping[categoryName] ?? "Neznáma kategória";
+}
